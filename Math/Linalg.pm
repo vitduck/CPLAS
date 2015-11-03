7e1833ee894913264d66473f24b32039c1925868
vitduck <levietduc@kaist.ac.kr>
2015-11-04 01:06:39 +0900

    Simply subroutines' argument handling with less dereferences

--- Math/Linalg.pm
+++ Math/Linalg.pm
@@ -1,322 +1,183 @@
-package Math; 
+package Math::Linalg; 
 
 use strict; 
 use warnings; 
 
-use Exporter   qw( import ); 
-use List::Util qw( sum ); 
+use Exporter; 
 
-use constant ARRAY => ref []; 
+use Fortran qw(fortran2perl); 
 
-# symbol 
-our @vector = qw( max_length print_vec product dot_product triple_product ); 
-our @matrix = qw( print_mat mat_dim det mat_add mat_mul hstack vstack transpose inverse ); 
-our @grid   = qw( mgrid ); 
+our @array  = qw(length max min sum product ascale dot triple vstack hstack print_array); 
+our @grid   = qw(mgrid); 
+our @matrix = qw(mat_dim det mat_add mscale mat_mul transpose inverse print_mat); 
 
-# default import 
-our @EXPORT = ( @vector, @matrix, @grid ); 
-
-# tag import 
+our @ISA         = qw(Exporter); 
+our @EXPORT      = (); 
+our @EXPORT_OK   = (@array, @grid, @matrix); 
 our %EXPORT_TAGS = ( 
-    vector => \@vector, 
-    matrix => \@matrix, 
+    array  => \@array, 
     grid   => \@grid,
+    matrix => \@matrix, 
 ); 
 
-#--------#
-# VECTOR # 
-#--------#
+#-------#
+# ARRAY # 
+#-------#
 
-# largest character length of vector's elements 
+# longest character length among array's elements 
+# override perl built-in length function 
 # args
-# -< $ref of vector 
-# return: 
+# -< array
+# return 
 # -> digit/character length 
-sub max_length { 
-    my @lists = @_; 
+sub length { 
+    my @array = @_; 
     
-    # max digit length 
-    my $length = ( sort {$b <=> $a} map length($_), @lists )[0]; 
+    # max digit/character length 
+    my $length = (sort { $b <=> $a } map length($_), @array)[0]; 
     
     return $length; 
 }
 
-# print vector 
+# min of array 
 # args 
-# -< filehandler 
-# -< ref of vector 
-# -< fortran format 
-# return: 
-# -> null 
-sub print_vec { 
-    my ($fh, $vec, $format)  = @_; 
+# -< array 
+# return 
+# -> min element
+sub min { 
+    my @array = @_; 
 
-    # figure out the format
-    my $perl_format = defined $format 
-    ? fortran_format($format) 
-    : join ' ', ("%s") x scalar(@$vec); 
+    my $min = (sort { $a <=> $b } @array)[0]; 
 
-    printf $fh "$perl_format\n", @$vec; 
-
-    return;  
+    return $min; 
 }
 
-# numerical product of all vector's elements 
+# max of array 
+# args 
+# -< array 
+# return 
+# -> max element
+sub max { 
+    my @array = @_; 
+
+    my $max = (sort { $b <=> $a } @array)[0]; 
+
+    return $max; 
+}
+
+# numerical sum of all array's elements 
 # arg: 
-# -< ref of vector 
+# -< array
+# return 
+# -> sum 
+sub sum { 
+    my @array = @_; 
+
+    my $sum = 0; 
+    for (@array) { $sum += $_ }
+
+    return $sum; 
+}
+
+# numerical product of all array's elements 
+# arg: 
+# -< array
 # return 
 # -> product 
 sub product { 
-    my ($vec) = @_; 
+    my @array = @_; 
 
     my $product = 1;  
-    for my $element (@$vec) { 
-        $product *= $element; 
-    }
+    for (@array) { $product *= $_ }
 
     return $product; 
 }
 
-# dot vector product
+# scale array 
+# args 
+# -< scaling factor 
+# -< array 
+# return 
+# -< scaled array 
+sub ascale { 
+    my ($scaling, @array) = @_; 
+    
+    return map $scaling*$_, @array; 
+} 
+
+# dot product
 # args
-# -< (scalar, ref of vector) pair or refs of two vectors 
+# -< ref of two arrays
 # return
-# -> ref of scaled vector or dot product 
-sub dot_product { 
-    my @vectors = grep { ref $_ eq ARRAY  } @_; 
+# -> dot product 
+sub dot { 
+    my ($array1, $array2) = @_; 
 
-    if ( @vectors == 1 ) { 
-        # scale vector by a scalar 
-        my ($scalar) = grep { ! ref $_ } @_; 
-        my $vector  = shift @vectors;  
+    # compatability check
+    unless ( @$array1 == @$array2 ) { die "IndexError: incompatible dimension\n" }
 
-        return [ map $scalar*$_, @$vector ]; 
-    } else {  
-        # dot product of two vector 
-        my ($vec1, $vec2) = @vectors; 
+    my $dot = 0;  
+    for (0..$#$array1) { 
+        $dot += $array1->[$_]*$array2->[$_]; 
+    }
 
-        # compatability check
-        unless ( @$vec1 == @$vec2 ) { die "IndexError: incompatible dimension\n" }
-
-        return sum(map { $vec1->[$_]*$vec2->[$_] } 0..$#$vec1); 
-    } 
+    return $dot; 
 }
 
-# triple vector product
+# triple product
 # args 
 # -< refs of three vectors 
 # return
-# -> volume of spanned by three vectors 
-sub triple_product { 
-    my ($vec1, $vec2, $vec3) = @_; 
-
+# -> triple product (volume)
+sub triple { 
     # vstack three vectors and 
-    my $mat = vstack([$vec1], [$vec2], [$vec3]); 
+    my @mat = vstack(@_); 
 
     # calculate the determinant of resulting 3x3 matrix
-    return det($mat); 
+    return det(@mat); 
 }
 
-#--------#
-# MATRIX #
-#--------#
-
-# print 2D matrix  
-# args 
-# -< ref of matrix
-# return
-# -> null 
-sub print_mat { 
-    my ($fh, $mat, $format) = @_; 
-    
-    for my $row (@$mat) { 
-        print_vec($fh, $row, $format);  
-    }
-
-    return; 
-}
-
-# dimesnion of arbitrary matrix 
-# args 
-# -< ref of matrix
-# return
-# -> array contains dimension of matrix
-sub mat_dim { 
-    my ($mat) = @_;  
-    
-    if (ref($mat->[0]) eq ARRAY) {  
-        my @shape; 
-        # recursive call 
-        push @shape, (scalar @$mat, mat_dim($mat->[0])); 
-        return @shape; 
-    } else { 
-        # halting condition 
-        # ref to 1d array is reached 
-        return scalar(@$mat); 
-    }
-}
-
-# hardcoded determinant of a 3x3 matrix 
-# args 
-# -< ref of a 3x3 matrix
-# return
-# -> det of matrix 
-sub det { 
-    my ($mat) = @_ ; 
-    
-    my $det =  $mat->[0][0]*$mat->[1][1]*$mat->[2][2] + 
-               $mat->[0][1]*$mat->[1][2]*$mat->[2][0] + 
-               $mat->[2][0]*$mat->[1][0]*$mat->[2][1] -
-               $mat->[0][2]*$mat->[1][1]*$mat->[2][0] - 
-               $mat->[0][1]*$mat->[1][0]*$mat->[2][2] - 
-               $mat->[0][0]*$mat->[1][2]*$mat->[2][1]; 
-
-    return $det; 
-}
-
-# add two matrices 
+# stack arrays vertically 
 # args
-# -< refs of two 2d matrices 
-# return
-# -> ref of sum 2d matrix
-sub mat_add { 
-    my ($mat1, $mat2) = @_; 
-
-    my $mat3;  
-    my ($mat1_nrow, $mat1_ncol) = mat_dim($mat1);  
-    my ($mat2_nrow, $mat2_ncol) = mat_dim($mat2);  
-
-    # compatability check
-    unless ( $mat1_nrow == $mat2_nrow ) { die "IndexError: incompatible dimension\n" }
-    unless ( $mat1_ncol == $mat2_ncol ) { die "IndexError: incompatible dimension\n" }
-
-    for my $i (0..$mat1_nrow-1) { 
-        for my $j (0..$mat1_ncol-1) { 
-            $mat3->[$i][$j] = $mat1->[$i][$j] + $mat2->[$i][$j]; 
-        }
-    }  
-
-    return $mat3;  
-}
-
-# stack matrices vertically 
-# args
-# -< list of refs of 2d matrices 
-# return: 
-# -> ref of stacked 2d matrix 
-sub vstack { 
-    my @mats = @_; 
-    
-    my $stacked_mat; 
-    for my $r2mat ( @mats ) { 
-        push @$stacked_mat, @$r2mat; 
-    }
-
-    return $stacked_mat; 
-}
-
-# stacked matrices horizontally 
-# args 
-# -< list of refs to 2d matrices 
+# -< refs of arrays
 # return 
-# -> ref of stacked 2d matrix 
+# -> stacked 2d matrix 
+sub vstack { 
+    my @vstack; 
+    for (@_) { 
+        push @vstack, $_; 
+    }
+    
+    return @vstack; 
+}
+
+# stack arrays horizontally 
+# args 
+# -< refs of arrays
+# return 
+# -> stacked 2d matrix 
 sub hstack { 
-    my @mats = @_; 
+    #1: stack arrays vertically 
+    my @vstack = vstack(@_); 
 
-    # 1: transposition 
-    my @trans_mats = map { transpose($_) } @mats; 
-
-    # 2: vtacking  
-    my $stacked_mat = vstack(@trans_mats); 
-
-    # 3: undo transposition 
-    return transpose($stacked_mat); 
+    #2: undo transposition 
+    return transpose(@vstack); 
 }
 
-# product of two 
-# matrices
+# print array 
 # args 
-# -< refs of two 2d matrices
-# return
-# -> ref of product matrix
-sub mat_mul { 
-    my $product;  
-    my @mats    = grep { ref $_ eq ARRAY  } @_; 
+# -< filehandler 
+# -< ref of array 
+# -< fortran equivalent format 
+# return 
+# -> null 
+sub print_array { 
+    my ($fh, $format, @array) = @_; 
 
-    if ( @mats == 1 ) { 
-        # scale the 2D matrix by a scalar 
-        my ($scalar) = grep { ! ref $_ } @_; 
-        my $mat      = shift @mats; 
+    my $perl_format = fortran2perl($format); 
+    printf $fh "$perl_format\n", @array;  
 
-        my ($nrow, $ncol) = mat_dim($mat); 
-        for my $i (0..$nrow-1) { 
-            for my $j ( 0..$ncol-1) { 
-                $product->[$i][$j] = $scalar*$mat->[$i][$j]; 
-            }
-        } 
-    } else { 
-        # 2D matrix multiplication 
-        my ($mat1, $mat2) = @mats; 
-        my ($mat1_nrow, $mat1_ncol) = mat_dim($mat1);  
-        my ($mat2_nrow, $mat2_ncol) = mat_dim($mat2);  
-       
-        # compatability check
-        unless ( $mat1_ncol == $mat2_nrow ) { die "IndexError: incompatible dimension\n" }
-
-        for my $i (0..$mat1_nrow-1) { 
-            for my $j (0..$mat2_ncol-1) { 
-                for my $k (0..$mat1_ncol-1) { 
-                    $product->[$i][$j] += $mat1->[$i][$k] * $mat2->[$k][$j]; 
-                }
-            }
-        }
-    }
-
-	return $product; 
-}
-
-# transpose 2d matrix 
-# args
-# -< ref to a 2D matrix
-# return
-# -> transposed matrix 
-sub transpose { 
-    my ($mat) = @_; 
-
-    my $transposed;       
-    my ($nrow, $ncol) = mat_dim($mat); 
-    for my $i (0..$ncol-1) { 
-        for my $j (0..$nrow-1) { 
-            $transposed->[$i][$j] = $mat->[$j][$i];  
-        }
-    }
-
-    return $transposed; 
-}
-
-# hardcoded inversion of a 3x3 matrix
-# args 
-# -< ref to a 3x3 matrix
-# return
-# -> inverse matrix 
-sub inverse { 
-    my ($mat) = @_; 
-    
-    my $inverse; 
-    my $det = det($mat); 
-    $inverse->[0][0] =  ($mat->[1][1]*$mat->[2][2] - $mat->[1][2]*$mat->[2][1])/$det; 
-    $inverse->[0][1] = -($mat->[0][1]*$mat->[2][2] - $mat->[0][2]*$mat->[2][1])/$det; 
-    $inverse->[0][2] =  ($mat->[0][1]*$mat->[1][2] - $mat->[0][2]*$mat->[1][1])/$det; 
-
-    $inverse->[1][0] = -($mat->[1][0]*$mat->[2][2] - $mat->[1][2]*$mat->[2][0])/$det; 
-    $inverse->[1][1] =  ($mat->[0][0]*$mat->[2][2] - $mat->[0][2]*$mat->[2][0])/$det; 
-    $inverse->[1][2] = -($mat->[0][0]*$mat->[1][2] - $mat->[0][2]*$mat->[1][0])/$det; 
-
-    $inverse->[2][0] =  ($mat->[1][0]*$mat->[2][1] - $mat->[1][1]*$mat->[2][0])/$det; 
-    $inverse->[2][1] = -($mat->[0][0]*$mat->[2][1] - $mat->[0][1]*$mat->[2][0])/$det; 
-    $inverse->[2][2] =  ($mat->[0][0]*$mat->[1][1] - $mat->[0][1]*$mat->[1][0])/$det; 
-    
-    return $inverse; 
+    return;  
 }
 
 #------# 
@@ -324,7 +185,7 @@ sub inverse {
 #------# 
 
 # generate cartesian grid 
-# similar to numpy mgrid 
+# crude imitation of numpy's mgrid()
 # args: 
 # -< xrange, i.e. 100:600:100 
 # -< yrange
@@ -341,7 +202,7 @@ sub mgrid {
     my $nx = int(($xr - $xl)/$xs); 
     my $ny = int(($yr - $yl)/$ys); 
 
-    # pseudo '2d' form of clum and row vector 
+    # pseudo '2d' form of column and row vectors 
     my @x = map $xl + $_ * $xs, 0..$nx-1;  
     my @y = map $yl + $_ * $ys, 0..$ny-1;  
     
@@ -365,30 +226,180 @@ sub mgrid {
 }
 
 #--------#
-# FORMAT # 
-#--------# 
+# MATRIX #
+#--------#
 
-# fortran-like format => perl format
-# crude implementation 
+# dimesnion of arbitrary matrix (recursive)
 # args 
-# -< fortran simple format  
-# return 
-# -> perl equivalent format
-sub fortran_format {  
-    my ($format) = @_; 
-
-    my $perl; 
-    my @fortran = ( $format =~ /(\d*)%([0-9.]*[dfs])/g ); 
-
-    while ( @fortran ) { 
-        # empty repition == 1
-        my $rep    = shift @fortran || 1; 
-        my $format = shift @fortran; 
-        #$perl     .= (join ' ', ("%$format")x$rep).' ';  
-        $perl     .= "%$format"x$rep; 
-    }
+# -< matrix
+# return
+# -> array dimension of matrix
+sub mat_dim { 
+    my @mat = @_;  
     
-    return $perl; 
+    if (ref($mat[0]) eq 'ARRAY') {  
+        my @shape; 
+        # recursive call 
+        push @shape, (scalar @mat, mat_dim(@{$mat[0]})); 
+        return @shape; 
+    } else { 
+        # halting condition 
+        # ref to 1d array is reached 
+        return scalar(@mat); 
+    }
+}
+
+# hardcoded determinant of a 3x3 matrix 
+# args 
+# -< a 3x3 matrix
+# return
+# -> det of matrix 
+sub det { 
+    my @mat = @_ ; 
+    
+    my $det =  $mat[0][0]*$mat[1][1]*$mat[2][2] + 
+               $mat[0][1]*$mat[1][2]*$mat[2][0] + 
+               $mat[0][2]*$mat[1][0]*$mat[2][1] -
+               $mat[0][2]*$mat[1][1]*$mat[2][0] - 
+               $mat[0][1]*$mat[1][0]*$mat[2][2] - 
+               $mat[0][0]*$mat[1][2]*$mat[2][1]; 
+
+    return $det; 
+}
+
+# add two matrices 
+# args
+# -< refs of 2d matrices 
+# return
+# -> 2d matrix whose element is sum of input matrices
+sub mat_add { 
+    my ($mat1, $mat2) = @_; 
+
+    my @mat_sum;  
+    my ($mat1_nrow, $mat1_ncol) = mat_dim(@$mat1);  
+    my ($mat2_nrow, $mat2_ncol) = mat_dim(@$mat2);  
+
+    # compatability check
+    if ($mat1_nrow != $mat2_nrow) { die "IndexError: incompatible dimension\n" }
+    if ($mat1_ncol != $mat2_ncol) { die "IndexError: incompatible dimension\n" }
+
+    for my $i (0..$mat1_nrow-1) { 
+        for my $j (0..$mat1_ncol-1) { 
+            $mat_sum[$i][$j] = $mat1->[$i][$j] + $mat2->[$i][$j]; 
+        }
+    }  
+
+    return @mat_sum;   
+}
+
+# scale matrix 
+# args 
+# -< scaling factor 
+# -< 2d matrix 
+# return  
+# -> scaled 2d matrix 
+sub mscale { 
+    my ($scaling, @mat) = @_; 
+    
+    my @scaled_mat; 
+
+    my ($nrow, $ncol) = mat_dim(@mat); 
+    for my $i (0..$nrow-1) { 
+        for my $j ( 0..$ncol-1) { 
+            $scaled_mat[$i][$j] = $scaling*$mat[$i][$j]; 
+        }
+    } 
+
+    return @scaled_mat; 
+}
+
+# product of two matrices
+# args 
+# -< refs of two 2d matrices
+# return
+# -> 2d product matrix
+sub mat_mul { 
+    my ($mat1, $mat2) = @_; 
+
+    my @product; 
+    
+    # compatability check
+    my ($mat1_nrow, $mat1_ncol) = mat_dim(@$mat1);  
+    my ($mat2_nrow, $mat2_ncol) = mat_dim(@$mat2);  
+    if ($mat1_ncol != $mat2_nrow) { die "IndexError: incompatible dimension\n" }
+
+    for my $i (0..$mat1_nrow-1) { 
+        for my $j (0..$mat2_ncol-1) { 
+            for my $k (0..$mat1_ncol-1) { 
+                $product[$i][$j] += $mat1->[$i][$k] * $mat2->[$k][$j]; 
+            }
+        }
+    }
+
+    return @product; 
+}
+
+# transpose 2d matrix 
+# args
+# -< ref to a 2D matrix
+# return
+# -> transposed matrix 
+sub transpose { 
+    my @mat = @_; 
+
+    my @transposed;       
+
+    my ($nrow, $ncol) = mat_dim(@mat); 
+    for my $i (0..$ncol-1) { 
+        for my $j (0..$nrow-1) { 
+            $transposed[$i][$j] = $mat[$j][$i];  
+        }
+    }
+
+    return @transposed; 
+}
+
+# hardcoded inversion of a 3x3 matrix
+# args 
+# -< a 3x3 matrix
+# return
+# -> inverse 3x3 matrix 
+sub inverse { 
+    my @mat = @_; 
+    
+    my @inverse; 
+    my $det = det(@mat); 
+
+    $inverse[0][0] =  ($mat[1][1]*$mat[2][2] - $mat[1][2]*$mat[2][1])/$det; 
+    $inverse[0][1] = -($mat[0][1]*$mat[2][2] - $mat[0][2]*$mat[2][1])/$det; 
+    $inverse[0][2] =  ($mat[0][1]*$mat[1][2] - $mat[0][2]*$mat[1][1])/$det; 
+
+    $inverse[1][0] = -($mat[1][0]*$mat[2][2] - $mat[1][2]*$mat[2][0])/$det; 
+    $inverse[1][1] =  ($mat[0][0]*$mat[2][2] - $mat[0][2]*$mat[2][0])/$det; 
+    $inverse[1][2] = -($mat[0][0]*$mat[1][2] - $mat[0][2]*$mat[1][0])/$det; 
+
+    $inverse[2][0] =  ($mat[1][0]*$mat[2][1] - $mat[1][1]*$mat[2][0])/$det; 
+    $inverse[2][1] = -($mat[0][0]*$mat[2][1] - $mat[0][1]*$mat[2][0])/$det; 
+    $inverse[2][2] =  ($mat[0][0]*$mat[1][1] - $mat[0][1]*$mat[1][0])/$det; 
+    
+    return @inverse; 
+}
+
+# print 2D matrix  
+# args 
+# -< filehandler 
+# -< fortran format 
+# -< 2d mat
+# return
+# -> null 
+sub print_mat { 
+    my ($fh, $format, @mat) = @_; 
+    
+    for my $array (@mat) { 
+        print_array($fh, $format, @$array);  
+    }
+
+    return; 
 }
 
 # last evaluated expression 
