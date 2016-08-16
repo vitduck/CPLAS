7c5f721e09c1604111c39b19878b49954ce3980a
vitduck <levietduc@kaist.ac.kr>
2016-08-16 12:01:56 +0900

    Rewrite VASP::KPOINTS class

--- VASP/KPOINTS.pm
+++ VASP/KPOINTS.pm
@@ -20,32 +20,89 @@ with 'IO::Read';
 # Moose attributes 
 has '_read_KPOINTS', ( 
     is       => 'ro', 
-    traits   => ['Hash'], 
+    isa      => 'ArrayRef[Str]', 
+    traits   => ['Array'], 
+    init_arg => undef, 
+
+    default  => sub ( $self ) { 
+        return $self->readline('KPOINTS'); 
+    },  
+    
+    # curried delegation 
+    handles  => { 'parse' => 'shift' }
+); 
+
+has 'comment', ( 
+    is       => 'ro', 
+    isa      => 'Str', 
     lazy     => 1, 
     init_arg => undef, 
 
     default  => sub ( $self ) { 
-        my $kpoint = {}; 
-        my @lines = $self->read_file('KPOINTS')->@*; 
-       
-        # first three lines are trivial
-        $kpoint->@{qw(comment nkpt mode)} = splice @lines, 0, 3; 
+        return $self->parse; 
+    } 
+); 
 
-        if ( $kpoint->{nkpt} == 0 ) { 
-            # automatic k-mesh generation ? 
-            $kpoint->{mesh}   = [ map split, shift @lines ];  
-            # k-mesh shift 
-            $kpoint->{shift}  = [ map split, shift @lines ];  
+has 'mode', (  
+    is       => 'ro', 
+    isa      => 'Int', 
+    lazy     => 1, 
+    init_arg => undef, 
+
+    default  => sub ( $self ) { 
+        return int($self->parse); 
+    } 
+); 
+
+has 'scheme', (  
+    is       => 'ro', 
+    isa      => 'Str', 
+    lazy     => 1, 
+    init_arg => undef, 
+
+    default  => sub ( $self ) { 
+        return ( 
+            $self->parse =~ /^M/ ?  
+            'Monkhorst-Pack' : 
+            'Gamma-centered'
+        ); 
+    } 
+); 
+
+has 'mesh', ( 
+    is       => 'ro', 
+    lazy     => 1, 
+    init_arg => undef, 
+
+    default  => sub ( $self ) { 
+        # automatic k-mesh generation ? 
+        if ( $self->mode == 0 ) { 
+            return [ map int, map split, $self->parse ]; 
+        # manual k-mesh 
+        } elsif ( $self->mode > 0 ) {  
+            my $k = []; 
+            while ( local $_ = $self->parse ) {   
+                push $k->@*, [(split)[0,1,2]],
+            }
+            return $k; 
+        # line-mode ( band calculation ) 
         } else { 
-            # manual k-mesh with weight ? 
-            $kpoint->{mesh} = [ map [ (split)[0,1,2] ],  @lines ];  
-        }
+            ...
+        } 
+    } 
+); 
 
-        return $kpoint; 
-    },  
+has 'shift', ( 
+    is       => 'ro', 
+    isa      => 'ArrayRef[Str]', 
+    lazy     => 1, 
+    init_arg => undef, 
 
-    # delegation 
-    handles  => { 'mesh' => [ get => 'mesh' ] }, 
+    default  => sub ( $self ) { 
+        if ( $self->mode == 0 ) {    
+            return [ map split, $self->parse ]; 
+        } 
+    }, 
 ); 
 
 has 'nkpt', ( 
@@ -55,13 +112,26 @@ has 'nkpt', (
     init_arg => undef, 
 
     default  => sub ( $self )  { 
-        return product($self->mesh->@*); 
+        # automatic k-mesh generation ? 
+        if ( $self->mode == 0 ) {  
+            return product($self->mesh->@*); 
+        } elsif ( $self->mode > 0 ) { 
+            return $self->mode; 
+        } else { 
+            ...
+        } 
     } 
 ); 
 
 # Moose methods
 sub BUILD ( $self, @args ) { 
+    # parsing order 
     $self->_read_KPOINTS; 
+    $self->comment; 
+    $self->mode; 
+    $self->scheme; 
+    $self->mesh; 
+    $self->shift; 
 } 
 
 # speed-up object construction 
